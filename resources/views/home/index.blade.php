677f66d806c6223dbc265ddac40cfcbbe33d575e
ljy <1745257632@qq.com>
2018-04-10 11:00:56 +0800

    第三次

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -26,8 +26,8 @@
 				<ul class="message-l">
 					<div class="topMessage">
 						<div class="menu-hd">
-							<a href="#" target="_top" class="h">亲，请登录</a>
-							<a href="#" target="_top">免费注册</a>
+							<a href="/home/Login" target="_top" class="h">亲，请登录</a>
+							<a href="/home/register" target="_top">免费注册</a>
 						</div>
 					</div>
 				</ul>
